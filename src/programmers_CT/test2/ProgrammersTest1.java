6c3b9241eb83733efc33e9dbb8a1576557f0b0a7
STGRAM\gffd9 <29088981l@gmail.com>
2025-05-27 00:08:48 +0900

    코딩테스트 문제풀이

--- src/programmers_CT/test2/ProgrammersTest1.java
+++ src/programmers_CT/test2/ProgrammersTest1.java
@@ -1,4 +1,27 @@
 package programmers_CT.test2;
 
+import java.util.Scanner;
+
 public class ProgrammersTest1 {
+    public static boolean solution(int x){
+        int num = x;
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        if(x % sum == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(solution(x));
+    }
+
 }
--- src/programmers_CT/test2/ProgrammersTest3.java
+++ src/programmers_CT/test2/ProgrammersTest3.java
@@ -1,4 +1,7 @@
 package programmers_CT.test2;
 
 public class ProgrammersTest3 {
+/*이 코드는 카카오 코딩테스트 유형 중 하나인 “그래프 구조 분석 문제”를 해결하는 풀이입니다.
+문제 자체는 주어지는 방향 그래프의 간선 목록에서 새롭게 생성된 노드와 세 가지 그래프 구조의 개수를 판별하는 것
+* */
 }
