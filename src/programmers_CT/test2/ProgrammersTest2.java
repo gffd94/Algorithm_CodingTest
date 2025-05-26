e4e890c6944f9d695258dbd249101029a6c64ea7
STGRAM\gffd9 <29088981l@gmail.com>
2025-05-27 00:08:48 +0900

    코딩테스트 문제풀이

--- src/programmers_CT/test2/ProgrammersTest2.java
+++ src/programmers_CT/test2/ProgrammersTest2.java
@@ -1,4 +1,42 @@
 package programmers_CT.test2;
 
+import java.util.Scanner;
+
 public class ProgrammersTest2 {
+
+    public static int solution(String s) {
+        int answer = 0;
+        int diff = 0;
+        char x = s.charAt(0);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (diff == 0) {
+                x = s.charAt(i);
+            }
+
+            if (s.charAt(i) == x) {
+                diff++;
+            } else {
+                diff--;
+            }
+
+            if (diff == 0) {
+                answer++;
+            }
+
+        }
+
+        if (diff != 0) {
+            answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(solution(str));
+    }
+
 }
--- src/programmers_CT/test2/ProgrammersTest4.java
+++ src/programmers_CT/test2/ProgrammersTest4.java
@@ -1,5 +1,25 @@
 package programmers_CT.test2;
 
-public class ProgrammersTest3 {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ProgrammersTest4 {
+/*이 코드는 프로그래머스 요격 시스템 문제로 알려진 “최소 요격 미사일 개수로 모든 타겟 요격하기” 문제에 대한 풀이입니다.
+이 문제는 Greedy (탐욕법) + 구간 정렬 문제의 대표적인 예제
+* */
+  public static int solution (int[][] targets) {
+      int answer = 0;
+      double lastEnd = -1.0;
+
+      Arrays.sort(targets, (a, b) -> Integer.compare(a[1], b[1]));
+
+      for (int[] target : targets) {
+          if (target[0] > lastEnd) {
+              lastEnd = target[1] - 0.1;
+              answer++;
+          }
+      }
+      return answer;
+  }
 
 }
