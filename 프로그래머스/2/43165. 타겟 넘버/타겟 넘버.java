01bf257074f38a685837641f98dfb64bee6612c5
SeungTae <122506273+gffd94@users.noreply.github.com>
2026-05-25 15:32:05 +0900

    [level 2] Title: 타겟 넘버, Time: 3.36 ms, Memory: 66.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,28 @@
+class Solution {
+    static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        
+        dfs(numbers, target, 0, 0);
+       
+         
+        return answer;
+        
+    }
+    
+    static void dfs (int[] numbers, int target, int index, int sum) {
+        // 종료조건
+        if(index == numbers.length){
+            if(sum == target) {
+                answer++;
+            }
+                return;
+        }
+        
+        // +
+        dfs(numbers, target, index + 1, sum+numbers[index]);
+        // -
+        dfs(numbers, target, index + 1, sum-numbers[index]);
+    }
+}
